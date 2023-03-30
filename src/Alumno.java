e39aad0839a19782c8d6a84f4ae18c2a99f92b9e
Stephanie <fp.daw.ies.ciudad.escolar@gmail.com>
2023-03-30 09:35:18 +0200

    Creación de la clase Alumno con su constructor

---
+++ src/Alumno.java
@@ -0,0 +1,12 @@
+
+public class Alumno {
+	String nombre;
+	String apellido;
+	String dni;
+	
+	Alumno(String nom, String ap, String dni){
+		this.nombre=nom;
+		this.apellido=ap;
+		this.dni=dni;
+	}
+}
