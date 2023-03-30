5d6f339e8f0a31c133f7a48897e68fdbdd99ee9e
Stephanie <fp.daw.ies.ciudad.escolar@gmail.com>
2023-03-30 09:39:55 +0200

    Instancia del objeto alumno

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Listado de alumnos 1º DAW");
 		
-		System.out.println("Alumno 1: ");
+		System.out.print("Alumno 1: ");
+		
+		Alumno a1 = new Alumno("Stephanie","Castro","54890676A");
+		
+		System.out.println(a1);
 
 	}
 
